48bf61e7d12ba49fb93bea3d8616c9377104c01d
na0260 <samjr.008@gmail.com>
2024-10-04 23:51:33 +0600

    title added

--- src/components/DataToolbar.jsx
+++ src/components/DataToolbar.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import {Box, Typography} from "@mui/material";
 import {GridToolbarContainer, GridToolbar, GridToolbarQuickFilter} from "@mui/x-data-grid";
 
 const DataToolbar = () => {
     return (
         <>
-            <GridToolbarContainer>
-                <GridToolbar/>
-                <GridToolbarQuickFilter/>
-            </GridToolbarContainer>
+            <Box display="flex" gap={5} justifyContent="space-between" alignItems="center" marginX={3} marginY={.5}>
+                <div>
+                    <Typography color="primary" variant="h6" component="h2">Books Data Table</Typography>
+                </div>
+                <GridToolbarContainer>
+                    <GridToolbar/>
+                    <GridToolbarQuickFilter/>
+                </GridToolbarContainer>
+            </Box>
+
         </>
     );
 };
